5f34bc18c0c7c80b8e30a3e3f425aaabf0e2710c
shabykov <shabykov12@gmail.com>
2020-10-15 23:45:47 +0300

    changed names

--- dags/import_recycle.py
+++ dags/import_recycle.py
@@ -23,13 +23,13 @@ args = {
 }
 
 TABLE_NAME = 'dashboards_recycle'
-COLUMNS = [
+TABLE_COLUMNS = [
     'updated_at', 'id_item', 'name', 'quantity',
     'store_id', 'is_selected_for_dashboard',
     'quantity_rto_plan', 'recycle_plan', 'rto_plan'
 ]
-CSV_FILE_RAW_PATH = 'data/dashboards_recycle.csv'
-CSV_FILE_CLEANED_PATH = 'data/dashboards_recycle_clean.csv'
+CSV_FILE_RAW_PATH = 'data/recycle.csv'
+CSV_FILE_CLEANED_PATH = 'data/recycle_clean.csv'
 CSV_FILE_PARAMS = {
     'sep': ';',
     'header': False,
@@ -40,35 +40,35 @@ CSV_FILE_PARAMS = {
 
 
 with DAG(
-        'import_dashboards_recycle',
+        'import_recycle',
         default_args=args,
-        schedule_interval='*/5 * * * *'
+        schedule_interval='*/50 * * * *'
 ) as dag:
-    extract_dashboards_recycle = ExtractFromPostgresOperator(
-        task_id='extract_dashboards_recycle',
+    extract_recycle = ExtractFromPostgresOperator(
+        task_id='extract_recycle',
         postgres_conn_id='postgres_default',
         sql='select {{ params.target_fields }} from {{ params.table_name }}',
-        params={'table_name': TABLE_NAME, 'target_fields': ', '.join(COLUMNS)},
+        params={'table_name': TABLE_NAME, 'target_fields': ', '.join(TABLE_COLUMNS)},
         pandas_sql_params={'chunksize': 100},
         csv_path=CSV_FILE_RAW_PATH,
         csv_params=CSV_FILE_PARAMS,
         depends_on_past=True,
         dag=dag)
 
-    transform_dashboards_recycle = TransformCSVFileOperator(
-        task_id='transform_dashboards_recycle',
+    transform_recycle = TransformCSVFileOperator(
+        task_id='transform_recycle',
         csv_file_from_path=CSV_FILE_RAW_PATH,
         csv_file_to_path=CSV_FILE_CLEANED_PATH,
-        csv_file_header=COLUMNS,
+        csv_file_header=TABLE_COLUMNS,
         csv_file_params=CSV_FILE_PARAMS,
         dag=dag)
 
-    load_dashboards_recycle = LoadFromCSVOperator(
-        task_id='load_dashboards_recycle',
+    load_recycle = LoadFromCSVOperator(
+        task_id='load_recycle',
         postgres_conn_id='postgres_default',
         csv_path=CSV_FILE_CLEANED_PATH,
         destination_table=TABLE_NAME,
-        target_fields=COLUMNS
+        target_fields=TABLE_COLUMNS
     )
 
-    extract_dashboards_recycle >> transform_dashboards_recycle >> load_dashboards_recycle
+    extract_recycle >> transform_recycle >> load_recycle
